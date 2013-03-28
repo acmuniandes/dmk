8c8af3957f01889ee9bba2b3116cac125b7064e8
fernandohur <fernandohur@gmail.com>
2013-03-28 17:56:20 -0400

    arreglado el /search con animacion

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,4 +2,5 @@ class Ad < ActiveRecord::Base
   attr_accessible :description, :image, :price, :title,:category_id
   belongs_to :user
   belongs_to :category
+  # TODO: cambiar el tipo de dato de :price a un string, para que el usuario pueda escribir 15/mes o algo asi
 end
