cb5f03d050f19c49f348a1d6cf6f6d54a8455051
fernandohur <fernandohur@gmail.com>
2013-04-02 23:33:28 -0500

    added footer and responsiveness

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -4,5 +4,8 @@ module FacebookHelper
     render :partial => "widgets/fb/like", :locals => {:url => url}
   end
 
+  def render_comments(url)
+    render :partial => "widgets/fb/comments" ,:locals => {:url => url}
+  end
 
 end
