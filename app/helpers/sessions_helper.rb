793f040c89890d641cffd210f80582808738d55c
fernandohur <fernandohur@gmail.com>
2013-04-11 18:03:21 -0500

    fixing login. There is still a weird log with no user bug

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
 
-  def get_user_id(request)
-    return request.session[:user_id]
+  def get_user_id
+    request.session[:user_id]
   end
 
   def is_signed_in
-    return request.session[:user_id] != nil
+    request.session[:user_id] != nil
   end
 
 end
